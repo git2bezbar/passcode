cb6d1564f158d4886541484bc9f5b38b9d4b8da2
Adem <ademduran6888@gmail.com>
2023-10-28 17:27:17 +0200

    fix: adapting key pad buttons size

--- src/components/KeyPadButton/index.tsx
+++ src/components/KeyPadButton/index.tsx
@@ -7,7 +7,7 @@ export default function KeyPadButton({ children, onClick }: ButtonProps) {
   return (
     <button
       onClick={onClick}
-      className="w-24 h-24 bg-very-light-blue text-white text-4xl rounded-xl border-4 drop-shadow-normal border-black flex justify-center items-center hover:scale-90 transition-all"
+      className="w-20 h-20 min-[390px]:w-24 min-[390px]:h-24 bg-very-light-blue text-white text-4xl rounded-xl border-4 drop-shadow-normal border-black flex justify-center items-center hover:scale-90 transition-all"
     >
       {children}
     </button>
